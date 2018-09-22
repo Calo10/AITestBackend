cbd7ca16d4e0e8ad8ded686616e469e75f6f2498
Jean Carlos <berrocal13@gmail.com>
2018-09-22 11:07:40 -0600

    Add class of extensions

---
+++ AITestBackend/Models/Extensions/ObjectExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AITestBackend.Models.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static bool IsNotNull(this object @object)
+        {
+            return IsNotNull<object>(@object);
+        }
+
+        public static bool IsNotNull<TObject>(this object @object)
+        {
+            return !Object.ReferenceEquals(@object, null);
+        }
+
+        public static bool IsNull(this object @object)
+        {
+            return IsNull<object>(@object);
+        }
+
+        public static bool IsNull<TObject>(this object @object)
+        {
+            return Object.ReferenceEquals(@object, null);
+        }
+
+    }
+}
